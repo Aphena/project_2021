70b3c520d3ff9437c4222c412a6672d817fa40af
Aphena <s1764254@sms.ed.ac.uk>
2021-02-25 18:59:33 +0800

    combining fastqc.md and cutadapt.md into single project,Rmd

--- multiqc.sh
+++ multiqc.sh
@@ -2,8 +2,8 @@
 
 conda update python
 python --version
-conda activate python3.8
 conda create -n python3.8 python=3.8
+conda activate python3.8
 conda install -c bioconda multiqc
 
 cd Desktop/raw_data/report
